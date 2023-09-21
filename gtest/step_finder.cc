625352f8de7a1e4709b8c43b5af166a6568cdb35
Thomas <thomas.sedlmair@yunextraffic.com>
2023-09-21 17:17:37 +0200

    added integer as variable in step

--- gtest/step_finder.cc
+++ gtest/step_finder.cc
@@ -4,15 +4,24 @@ extern "C" {
   #include "step_matcher.h"
 }
 
-TEST(step_matcher, is_step_1)
+TEST(step_matcher, is_step)
 {
   EXPECT_TRUE(is_step("foo", "foo"));
 }
-TEST(step_matcher, is_step_2)
+TEST(step_matcher, is_step_with_string)
 {
   EXPECT_TRUE(is_step("a step with a {string} inside", "a step with a \"string value\" inside"));
   EXPECT_TRUE(is_step("{string} at the beginning", "\"string value\" at the beginning"));
   EXPECT_TRUE(is_step("or a trailing {string}", "or a trailing \"string value\""));
   EXPECT_TRUE(is_step("{string} at the beginning, {string} inside and a trailing {string}", 
                       "\"string value\" at the beginning, \"string value\" inside and a trailing \"string value\""));
+}
+TEST(step_matcher, is_step_with_int)
+{
+  EXPECT_TRUE(is_step("a step with a {int} inside", "a step with a 123 inside"));
+  EXPECT_TRUE(is_step("a step with a {int} inside", "a step with a 123.456 inside"));
+  EXPECT_TRUE(is_step("{int} at the beginning", "15 at the beginning"));
+  EXPECT_TRUE(is_step("or a trailing {int}", "or a trailing 55.123"));
+  EXPECT_TRUE(is_step("{int} at the beginning, {int} inside and a trailing {int}", 
+                      "12.12 at the beginning, 1212 inside and a trailing 9999.1"));
 }
--- src/step_matcher.c
+++ src/step_matcher.c
@@ -9,12 +9,12 @@ typedef enum {
   PH_INVALID
 } placeholder_type;
 
-
 placeholder_type get_placehoder(const char* begin, const char* end)
 {
   switch (*begin)
   {
   case 's': return memcmp("tring", begin+1, end-begin) == 0 ? PH_STRING : PH_INVALID;
+  case 'i': return memcmp("nt", begin+1, end-begin) == 0 ? PH_INT : PH_INVALID;
   default: return PH_INVALID;
   }
 }
@@ -22,9 +22,14 @@ placeholder_type get_placehoder(const char* begin, const char* end)
 // TODO get never closing bracket!
 void advance_after(char c, const char** str)
 {
-  while(c != **str) /*&& str[pos] != '\0' or '\n' */
+  while(c != **str)
   {
     (*str)++;
+    if (c == '\0' || c == '\n') 
+    {
+      fprintf(stderr, "Missing %c in StepLine.\n", c);
+      return;
+    }
   }
   (*str)++;
 }
@@ -38,19 +43,56 @@ placeholder_type placeholder(const char** str)
   return get_placehoder(var_begin, var_end);
 }
 
+bool is_digit(char c)
+{
+  return c >= '0' && c <= '9';
+}
+
+void integer(const char** str)
+{
+  bool dot = false; 
+  while (**str != ' ' && **str != '\0' && **str != '\n')
+  {
+    if (**str == '.') 
+    {
+      if (dot) 
+      {
+        fprintf(stderr, "More than one dot in number.\n");
+      }
+      dot = true;
+      (*str)++;
+    }
+    if (is_digit(**str)) 
+    {
+      (*str)++;
+    }
+    else 
+    {
+      fprintf(stderr, "Number contains non digit character.\n");
+    }
+  }
+}
+
+void string(const char** str) 
+{
+  if (**str != '"') 
+  {
+    fprintf(stderr, "Expect '\"' for string value.\n");
+  }
+  (*str)++;
+  advance_after('"', str);
+}
+
 bool is_step(const char* defined, const char* feature)
 {
   while (*defined != '\0' && *feature != '\0')
   {
     if (*defined == '{') {
       placeholder_type ph = placeholder(&defined);
-
-      if (ph == PH_STRING) {
-        if (*feature != '"') {
-          fprintf(stderr, "Expect '\"' for string value.\n");
-        }
-        feature++;
-        advance_after('"', &feature);
+      switch (ph) 
+      {
+        case PH_STRING: string(&feature);
+        break; case PH_INT: integer(&feature);
       }
     }
 
