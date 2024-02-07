8e050d00be5665791fd127bc3de7d2c7d75794be
thomas <thomas.sedlmair@googlemail.com>
2024-02-07 17:50:58 +0100

    update

--- src/cwt/context.hpp
+++ src/cwt/context.hpp
@@ -41,10 +41,6 @@ class context_type
   {
     c_model() = default;
 
-    value_model(const T& arg) : m_value{arg}
-    {
-    }
-
     template <typename... Args>
     c_model(Args&&... args) : m_value(std::forward<Args>(args)...){};
 
