fb4add068fc79deff63ab655e6e61aa4e0cc7910
Thomas Sedlmair <thomas.sedlmair@validas.de>
2024-07-30 09:25:04 +0200

    update

---
+++ src/ast/test_runner.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ast.hpp"
+#include "registry.hpp"
+#include "../step_finder.hpp"
+
+namespace cuke::internal
+{
+class test_runner
+{
+ public:
+  void visit(const cuke::ast::scenario_node& scenario)
+  {
+    for (const cuke::ast::step_node& step : scenario.steps())
+    {
+      cuke::internal::step_finder finder(step.name());
+      auto it = finder.find(cuke::registry::steps().begin(),
+                            cuke::registry::steps().end());
+      if (it != cuke::registry::steps().end())
+      {
+        it->call(finder.values());
+      }
+    }
+  }
+  void visit(const cuke::ast::scenario_outline_node&) {}
+};
+
+}  // namespace cuke::internal
--- src/step_finder.hpp
+++ src/step_finder.hpp
@@ -1,4 +1,7 @@
+#pragma once 
+
 #include <string_view>
+#include <algorithm>
 
 #include "value.hpp"
 #include "scanner.hpp"
@@ -11,10 +14,21 @@ class step_finder
  public:
   step_finder(std::string_view defined, std::string_view feature);
   step_finder(std::string_view feature);
-  
+
   void reset_with_next_step(std::string_view defined) noexcept;
   [[nodiscard]] bool step_matches();
-  [[nodiscard]] const cuke::value_array& values() const noexcept; 
+  [[nodiscard]] const cuke::value_array& values() const noexcept;
+
+  template <typename Iterator>
+  Iterator find(Iterator first, Iterator last)
+  {
+    return std::find_if(first, last,
+                        [this](const cuke::internal::step& s)
+                        {
+                          reset_with_next_step(s.definition());
+                          return step_matches();
+                        });
+  }
 
  private:
   [[nodiscard]] bool parameter_matches_value(token_type parameter,
@@ -25,6 +39,7 @@ class step_finder
   [[nodiscard]] std::pair<token, token> next();
   [[nodiscard]] cuke::value create_table();
   void skip_linebreaks();
+
  private:
   scanner m_defined{""};
   scanner m_feature{""};
