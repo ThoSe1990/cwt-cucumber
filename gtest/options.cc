51db95a7e553417c195c0c52714064cbd6b54044
Thomas Sedlmair <thomas.sedlmair@validas.de>
2024-08-05 14:01:50 +0200

    added tests for options

--- gtest/options.cc
+++ gtest/options.cc
@@ -1,4 +1,6 @@
 #include <format>
+#include <string_view>
+
 #include <gtest/gtest.h>
 
 #include "../src/options.hpp"
@@ -27,4 +29,65 @@ TEST(options, file_path_does_exist)
   cuke::internal::terminal_arguments targs(argc, argv);
   ASSERT_FALSE(targs.get_options().files.empty());
   EXPECT_EQ(targs.get_options().files[0].path, std::string(argv[1]));
+}
+namespace details
+{
+static std::string remove_trailing_char(std::string_view str, std::size_t n)
+{
+  return std::string(str.substr(0, str.size() - n));
+}
+}  // namespace details
+TEST(options, file_path_does_exist_w_line)
+{
+  std::string path =
+      std::format("{}/test_files/any.feature:3", unittests::test_dir());
+  const char* argv[] = {"program", path.c_str()};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  cuke::internal::terminal_arguments targs(argc, argv);
+  ASSERT_FALSE(targs.get_options().files.empty());
+  EXPECT_EQ(targs.get_options().files[0].path,
+            details::remove_trailing_char(argv[1], 2));
+  ASSERT_FALSE(targs.get_options().files[0].lines_to_compile.empty());
+  EXPECT_EQ(targs.get_options().files[0].lines_to_compile.at(0), 3);
+}
+TEST(options, file_path_does_exist_w_lines)
+{
+  std::string path = std::format("{}/test_files/any.feature:3:123:9999",
+                                 unittests::test_dir());
+  const char* argv[] = {"program", path.c_str()};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  cuke::internal::terminal_arguments targs(argc, argv);
+  ASSERT_FALSE(targs.get_options().files.empty());
+  EXPECT_EQ(targs.get_options().files[0].path,
+            details::remove_trailing_char(argv[1], 11));
+  ASSERT_EQ(targs.get_options().files[0].lines_to_compile.size(), 3);
+  EXPECT_EQ(targs.get_options().files[0].lines_to_compile.at(2), 3);
+  EXPECT_EQ(targs.get_options().files[0].lines_to_compile.at(1), 123);
+  EXPECT_EQ(targs.get_options().files[0].lines_to_compile.at(0), 9999);
+}
+TEST(options, tag_expression_1)
+{
+  const char* argv[] = {"program", "-t", "@tag1 or @tag2"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  cuke::internal::terminal_arguments targs(argc, argv);
+  ASSERT_FALSE(targs.get_options().tags.empty());
+  EXPECT_TRUE(
+      targs.get_options().tags.evaluate(std::vector{std::string{"@tag1"}}));
+  EXPECT_TRUE(
+      targs.get_options().tags.evaluate(std::vector{std::string{"@tag2"}}));
+  EXPECT_FALSE(
+      targs.get_options().tags.evaluate(std::vector{std::string{"@tag3"}}));
+}
+TEST(options, tag_expression_2)
+{
+  const char* argv[] = {"program", "--tags", "@tag1 or @tag2"};
+  int argc = sizeof(argv) / sizeof(argv[0]);
+  cuke::internal::terminal_arguments targs(argc, argv);
+  ASSERT_FALSE(targs.get_options().tags.empty());
+  EXPECT_TRUE(
+      targs.get_options().tags.evaluate(std::vector{std::string{"@tag1"}}));
+  EXPECT_TRUE(
+      targs.get_options().tags.evaluate(std::vector{std::string{"@tag2"}}));
+  EXPECT_FALSE(
+      targs.get_options().tags.evaluate(std::vector{std::string{"@tag3"}}));
 }
