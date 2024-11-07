f774e29205fb8dd5a93ea7f816e24a198d752ac7
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2024-11-07 14:09:43 +0100

    added any.hpp

--- src/get_args.hpp
+++ src/get_args.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <memory>
 #include <string_view>
 #include <unordered_map>
 
+#include "any.hpp"
 #include "param_info.hpp"
 #include "value.hpp"
 
@@ -37,61 +37,7 @@ template <typename T>
 static constexpr bool has_conversion_v =
     has_conversion<conversion_impl<T>>::value;
 
-// TODO: move this into any.hpp
-class any_base
-{
- public:
-  virtual ~any_base() = default;
-  virtual std::unique_ptr<any_base> clone() const = 0;
-};
 
-template <typename T>
-class any_impl : public any_base
-{
- public:
-  any_impl(T value) : m_value(std::move(value)) {}
-
-  std::unique_ptr<any_base> clone() const override
-  {
-    return std::make_unique<any_impl<T>>(m_value);
-  }
-
-  T get() const { return m_value; }
-
-  template <typename U>
-  U convert() const
-  {
-    return static_cast<U>(m_value);
-  }
-
- private:
-  T m_value;
-};
-
-class any
-{
- public:
-  template <typename T>
-  any(T value) : m_data(std::make_unique<any_impl<T>>(std::move(value)))
-  {
-  }
-
-  any(const any& other) : m_data(other.m_data->clone()) {}
-
-  template <typename T>
-  operator T() const
-  {
-    auto ptr = static_cast<any_impl<T>*>(m_data.get());
-    if (!ptr)
-    {
-      throw std::runtime_error("Invalid cast.");
-    }
-    return ptr->get();
-  }
-
- private:
-  std::unique_ptr<any_base> m_data;
-};
 
 // TODO: create a macro e.g. CUKE_PARAM_ARG(idx) for this
 static const cuke::value& get_param_value(
