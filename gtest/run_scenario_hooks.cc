d7b0c5990ee227508d767f17ac64ba47e8ede7db
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2025-06-23 10:22:50 +0200

    added tests

--- gtest/run_scenario_hooks.cc
+++ gtest/run_scenario_hooks.cc
@@ -3,6 +3,7 @@
 
 #include "../src/test_runner.hpp"
 #include "../src/parser.hpp"
+#include "test_results.hpp"
 
 using namespace cuke::internal;
 
@@ -247,4 +248,127 @@ TEST_F(run_scenario_hook_skip, run_taggeed_example)
   p.for_each_scenario(runner);
 
   EXPECT_EQ(run_scenario_hook_skip::calls, 1);
+}
+
+class run_scenario_hook_fail_scenario: public ::testing::Test
+{
+ protected:
+  void SetUp() override
+  {
+    calls = 0;
+    cuke::registry().clear();
+
+    cuke::registry().push_step(
+      step_definition([](const cuke::value_array&, const auto&, const auto&, const auto&)
+             { ++calls; }, "a step"));
+
+    cuke::registry().push_hook_before(hook([]() { /*TODO: implement cuke::fail_scenario() here*/ }, "@will_fail"));
+  }
+  static std::size_t calls;
+};
+std::size_t run_scenario_hook_fail_scenario::calls = 0;
+
+TEST_F(run_scenario_hook_fail_scenario, set_scenario_to_failed_1)
+{
+  const char* script = R"*(
+    Feature: a feature 
+
+    @will_fail
+    Scenario: Hook calls cuke::scenario_fail();
+    Given a step 
+  )*";
+
+  cuke::parser p;
+  p.parse_script(script);
+  cuke::test_runner runner;
+  p.for_each_scenario(runner);
+
+  EXPECT_EQ(run_scenario_hook_fail_scenario::calls, 0);
+  EXPECT_EQ(cuke::results::test_results().scenarios_passed(), 0);
+  EXPECT_EQ(cuke::results::test_results().scenarios_failed(), 1);
+  EXPECT_EQ(cuke::results::test_results().steps_skipped(), 1);
+}
+
+TEST_F(run_scenario_hook_fail_scenario, set_scenario_to_failed_2)
+{
+  const char* script = R"*(
+    Feature: a feature 
+
+    @will_fail
+    Scenario: Hook calls cuke::scenario_fail();
+    Given a step 
+    Scenario: Will not fail
+    Given a step 
+  )*";
+
+  cuke::parser p;
+  p.parse_script(script);
+  cuke::test_runner runner;
+  p.for_each_scenario(runner);
+
+  EXPECT_EQ(run_scenario_hook_fail_scenario::calls, 1);
+  EXPECT_EQ(cuke::results::test_results().scenarios_passed(), 1);
+  EXPECT_EQ(cuke::results::test_results().scenarios_failed(), 1);
+  EXPECT_EQ(cuke::results::test_results().steps_skipped(), 1);
+  EXPECT_EQ(cuke::results::test_results().steps_passed(), 1);
+}
+
+class run_scenario_hook_fail_step: public ::testing::Test
+{
+ protected:
+  void SetUp() override
+  {
+    calls = 0;
+    cuke::registry().clear();
+
+    cuke::registry().push_step(
+      step_definition([](const cuke::value_array&, const auto&, const auto&, const auto&)
+             { ++calls; }, "a step"));
+
+    cuke::registry().push_hook_before_step(hook([]() { /*TODO: implement cuke::fail_step() here*/ }));
+  }
+  static std::size_t calls;
+};
+std::size_t run_scenario_hook_fail_step::calls = 0;
+
+TEST_F(run_scenario_hook_fail_step, set_step_to_failed_1)
+{
+  const char* script = R"*(
+    Feature: a feature 
+
+    Scenario: A Scenario
+    Given a step 
+  )*";
+
+  cuke::parser p;
+  p.parse_script(script);
+  cuke::test_runner runner;
+  p.for_each_scenario(runner);
+
+  EXPECT_EQ(run_scenario_hook_fail_step::calls, 0);
+  EXPECT_EQ(cuke::results::test_results().scenarios_passed(), 0);
+  EXPECT_EQ(cuke::results::test_results().scenarios_failed(), 1);
+  EXPECT_EQ(cuke::results::test_results().steps_failed(), 1);
+}
+TEST_F(run_scenario_hook_fail_step, set_step_to_failed_2)
+{
+  const char* script = R"*(
+    Feature: a feature 
+
+    Scenario: A Scenario
+    Given a step 
+    Scenario: A Scenario
+    Given a step 
+    And a step 
+  )*";
+
+  cuke::parser p;
+  p.parse_script(script);
+  cuke::test_runner runner;
+  p.for_each_scenario(runner);
+
+  EXPECT_EQ(run_scenario_hook_fail_step::calls, 0);
+  EXPECT_EQ(cuke::results::test_results().scenarios_passed(), 0);
+  EXPECT_EQ(cuke::results::test_results().scenarios_failed(), 2);
+  EXPECT_EQ(cuke::results::test_results().steps_failed(), 3);
 }
