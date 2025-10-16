e8de91a36901e4e8a23021ee8396b778a0fa05c3
Thomas Sedlmair <thomas.sedlmair@googlemail.com>
2025-10-16 21:36:23 +0200

    cleanup

--- gtest/run_scenarios_dry_run.cc
+++ gtest/run_scenarios_dry_run.cc
@@ -142,9 +142,6 @@ TEST_F(run_scenarios_dry_run, run_scenario_dry_w_undefined_step)
   p.for_each_scenario(runner);
   EXPECT_EQ(run_scenarios_dry_run::call_count, 0);
 
-  using namespace cuke::results;
-  EXPECT_EQ(final_result(), test_status::failed);
-
 #ifdef UNDEFINED_STEPS_ARE_A_FAILURE
   using namespace cuke::results;
   EXPECT_EQ(final_result(), test_status::failed);
@@ -152,6 +149,7 @@ TEST_F(run_scenarios_dry_run, run_scenario_dry_w_undefined_step)
   const auto& scenarios = features_back().scenarios;
   EXPECT_EQ(scenarios.at(0).status, test_status::failed);
 #else 
+  using namespace cuke::results;
   const auto& scenarios = features_back().scenarios;
   EXPECT_EQ(scenarios.at(0).status, test_status::skipped);
 
